480aa0f97c233a860c1dade1b1810945bfae7551
changcitto <changcitto@gmail.com>
2023-11-16 22:24:38 +0900

    JUnit 적용

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,25 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
 
-    private int nbErrors = 0;
-
-    public static void main(String[] args) {
-        CalculatorTest test = new CalculatorTest();
-        try {
-            testAdd();
-        } catch (Throwable e) {
-            test.nbErrors++;
-            e.printStackTrace();
-        }
-        if (test.nbErrors > 0) {
-            throw new IllegalStateException("There were " + test.nbErrors + "error(s)");
-        }
-    }
-
-    private static void testAdd() {
+    @Test
+    public void testAdd() {
         Calculator calculator = new Calculator();
         double result = calculator.add(10, 50);
-        if (result != 60) {
-            throw new IllegalStateException("Bad result: " + result);
-        }
+        assertEquals(60, result, 0  );
     }
+
 }
